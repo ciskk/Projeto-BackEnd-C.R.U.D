d1c18a15c2d31a54dd60f6404e112f519bf865b0
Francisco Cândido Pereira Pontes <francisco.pontes00@aluno.ifce.edu.br>
2025-12-08 22:27:13 +0000

    ㅤ

--- schema.py
+++ schema.py
@@ -1,19 +1,17 @@
 from pydantic import BaseModel, ConfigDict, EmailStr
 
-# Schema base para receber dados (tem senha)
 class BaseUsuario(BaseModel):
     username: str
-    email: str  # Se tiver validator instalado, pode usar EmailStr
+    email: str  
     password: str
 
-# Schema público para devolver dados (SEM senha)
+
 class UsuarioPublic(BaseModel):
     id: int
     username: str
     email: str
     model_config = ConfigDict(from_attributes=True)
-    
-# Schema genérico de Usuário (caso precise no futuro)
+
 class Usuario(BaseUsuario):
     id: int
     model_config = ConfigDict(from_attributes=True)
